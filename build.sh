12182e69fcaef735ff885ad1e80a8ceea63cedf1
David Tomberlin <siyegen@gmail.com>
2016-04-04 13:31:43 -0700

    add build command for g++ since I keep forgetting what it is

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+echo "building game"
+
+g++ -std=c++11 -W -Wall -Wextra -pedantic -I includes ./*.cpp -o game \
+ -lglfw3 -framework Cocoa -framework OpenGL -framework IOKit \
+ -framework CoreVideo -lGLEW
