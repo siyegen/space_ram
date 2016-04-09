479e3af13c0dd0e9b5ab828b0608981a2e7dc884
David Tomberlin <siyegen@gmail.com>
2016-04-08 17:21:16 -0700

    skeleton for config and simple level tool

---
+++ level_gen_tool/gulpfile.js
@@ -0,0 +1,24 @@
+var gulp       = require('gulp');
+var gutil      = require('gulp-util');
+var browserify = require('gulp-browserify');
+var connect    = require('gulp-connect');
+
+gulp.task('js', function () {
+  return gulp.src("js/main.js")
+    .pipe(browserify().on('error', gutil.log))
+    .pipe(gulp.dest('./public'));
+});
+
+gulp.task('sass', function () {
+  return gulp.src('styles/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./public'));
+});
+
+gulp.task('default', function() {
+  gulp.watch('js/*.js', ['js']);
+  connect.server({
+    root: 'public',
+    port: 9929
+  });
+});
